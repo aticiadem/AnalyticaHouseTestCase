e615293ccba2fa66bf441f25d97735826957a710
adematici <adematicis41@gmail.com>
2021-10-25 20:05:10 +0300

    screens added

---
+++ app/src/main/java/com/analyticahouse/test/ui/screens/favorite/FavoritePlayersScreen.kt
@@ -0,0 +1,8 @@
+package com.analyticahouse.test.ui.screens.favorite
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FavoritePlayersScreen() {
+
+}
---
+++ app/src/main/java/com/analyticahouse/test/ui/screens/team/TeamDetailScreen.kt
@@ -0,0 +1,8 @@
+package com.analyticahouse.test.ui.screens.team
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TeamDetailScreen() {
+
+}
