62321c1b1197b6859b3ec90d7bb11ebeafaa09b3
adematici <adematicis41@gmail.com>
2021-10-26 23:00:40 +0300

    main activity di update

--- app/src/main/java/com/analyticahouse/test/MainActivity.kt
+++ app/src/main/java/com/analyticahouse/test/MainActivity.kt
@@ -9,7 +9,9 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.analyticahouse.test.ui.theme.AnalyticaHouseTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
